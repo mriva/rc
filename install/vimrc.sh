5e039d47395678f62c260c40f983c6807c538918
Matteo Riva <muradin@gmail.com>
2021-03-24 23:31:21 +0100

    Cleanup vimrc install script

--- install/vimrc.sh
+++ install/vimrc.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-
 mkdir -p ~/.vim/tmp
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -sfLo ~/.vimrc https://raw.githubusercontent.com/mriva/rc/master/vimrc
 vim -es -u .vimrc -i NONE -c "PlugInstall" -c "qa"
-
-echo "Done."
