9eb2ca009aac5c62e2c04dad6653fe70879a0871
Matteo Riva <muradin@gmail.com>
2021-03-25 00:12:57 +0100

    Fix vimrc install script when overwriting existing installation

--- install/vimrc.sh
+++ install/vimrc.sh
@@ -1,3 +1,4 @@
+rm -rf ~/.vim/plugged
 mkdir -p ~/.vim/tmp
 curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -sfLo ~/.vimrc https://raw.githubusercontent.com/mriva/rc/master/vimrc
