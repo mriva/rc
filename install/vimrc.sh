1d7c0a0f175cc3439abaf7030f029db633559a2a
Matteo Riva <muradin@gmail.com>
2021-03-24 23:07:23 +0100

    Autoinstall plugins

--- install/vimrc.sh
+++ install/vimrc.sh
@@ -3,3 +3,4 @@
 mkdir -p ~/.vim/tmp
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vimrc https://raw.githubusercontent.com/mriva/rc/master/vimrc
+vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
